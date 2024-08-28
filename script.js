56588e8d21a1572086b330f05e5837131702df6c
shivamparkar <parkarshivam@gmail.com>
2024-08-28 23:02:26 +0530

    updated

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let css = document.querySelector("h3");   //we are selecting query 
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
 
+console.log("Color1:", color1); 
+console.log("Color2:", color2); 
+console.log("Body:", body); 
+
+
 const setGradient = () => {
-    body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
-}                                    //using css and creating a function to change gradient color
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+};
 
-color1.addeventListener("input", setGradient()); 
 
-color2.addeventListener("input", setGradient());
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
